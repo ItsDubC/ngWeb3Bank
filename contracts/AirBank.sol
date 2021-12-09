8b4dbb7f676f83f143463b7b8d446379507f4d72
ItsDubC <itsdubc@gmail.com>
2021-12-08 19:50:22 -0600

    Implemented staked & unstake functions.  Started implementing reward issuance.

--- contracts/AirBank.sol
+++ contracts/AirBank.sol
@@ -23,14 +23,35 @@ contract AirBank {
     }
 
     function stakeUsdcTokens(uint256 amount) public {
+        require(amount > 0, 'Amount must be greater than 0');
 
+        _usdc.transferFrom(msg.sender, address(this), amount);
+
+        _stakedBalance[msg.sender] += amount;
+
+        if (!_hasStaked[msg.sender]) {
+            _stakers.push(msg.sender);
+        }
+
+        _hasStaked[msg.sender] = true;
+        _isCurrentlyStaked[msg.sender] = true;
     }
 
     function unstakeAllUsdcTokens() public {
-        
+        uint256 stakedBalance = _stakedBalance[msg.sender];
+
+        require(stakedBalance > 0, 'No mUSDC balance to unstake');
+
+        _usdc.transfer(msg.sender, stakedBalance);
+
+        _stakedBalance[msg.sender] = 0;
+        _isCurrentlyStaked[msg.sender] = false;
     }
 
     function issueAbrtTokenRewards() public {
+        require(msg.sender == _owner, 'Only AirBank can issue ABRT reward tokens');
 
+        // Reward 5% staked amount
+         
     }
 }
