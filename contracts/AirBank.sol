2c6e9f11e61c8e29cfa41c1816a941e6b40796c2
ItsDubC <itsdubc@gmail.com>
2021-12-08 19:57:07 -0600

    Implementing reward issuance.

--- contracts/AirBank.sol
+++ contracts/AirBank.sol
@@ -51,7 +51,14 @@ contract AirBank {
     function issueAbrtTokenRewards() public {
         require(msg.sender == _owner, 'Only AirBank can issue ABRT reward tokens');
 
-        // Reward 5% staked amount
-         
+        // Reward 5% staked mUSDC amount in ABRT
+         for (uint256 i = 0; i < _stakers.length; i++) {
+             address recipient = _stakers[i];
+
+             if (_stakedBalance[recipient] > 0) {
+                 uint256 abrtReward = _stakedBalance[recipient] * 1.05; // transfer(address to, uint256 value) public returns (bool success) {
+                 _abrt.transfer(recipient, abrtReward);
+             }
+         }
     }
 }
