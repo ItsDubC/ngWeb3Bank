c305aace593be39f4192b53f9fb562b845eab4f5
ItsDubC <itsdubc@gmail.com>
2021-12-11 06:31:42 -0600

    Added Angular Material & applied default theme background.  Setup header component & stubbed out BalanceSummary and Stake components.

---
+++ AirBank/src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Web3ContractService } from 'src/app/services/web3-contract.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  accountId: string = '';
+
+  constructor(
+    private web3ContractService: Web3ContractService
+  ) { }
+
+  ngOnInit(): void {
+    this.accountId = 'testAcctId';
+  }
+
+}
